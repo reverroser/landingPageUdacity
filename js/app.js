7a9147e7fd939251164ce56020af511d36317a2f
Roser Reverte <reverroser@gmail.com>
2020-03-12 20:57:26 +0100

    set active class on scroll and scroll to anchor ID

--- js/app.js
+++ js/app.js
@@ -14,28 +14,18 @@
 */
 
 /**
- * Define Global Variables
+ * Define Global constiables
  * 
 */
-
-
+const pageSections = document.querySelectorAll('section');
+const navbarList = document.getElementById('navbar__list');
 
 /**
- * End Global Variables
+ * End Global constiables
  * Start Helper Functions
  *
 */
 
-function buildNavbar() {
-    var pageSections = document.querySelectorAll('section');
-    var navbarList = document.getElementById('navbar__list');
-    for (var i = 0; i < pageSections.length; i++) {
-        var navItem = document.createElement('li');
-        var navLabel = pageSections[i].dataset.nav;
-        navItem.innerHTML = navLabel;
-        navbarList.appendChild(navItem);
-    }
-};
 
 /**
  * End Helper Functions
@@ -44,15 +34,43 @@ function buildNavbar() {
 */
 
 // build the nav
-
-buildNavbar();
-
+const buildNavbar = () => {
+    for (let section of pageSections) {
+        const navItem = document.createElement('li');
+        const navItemLink = document.createElement('a');
+        const navLabel = section.dataset.nav;
+        navItemLink.className = 'menu__link';
+        navItemLink.innerHTML = navLabel;
+        navItemLink.addEventListener('click', () => scrollToAnchor(section.id));
+        navItem.appendChild(navItemLink);
+        navbarList.appendChild(navItem);
+    }
+};
 
 // Add class 'active' to section when near top of viewport
+const setActiveSectionOnScroll = () => {
+    for (let section of pageSections) {
+        const { top } = section.getBoundingClientRect();
+        const offset = section.offsetHeight;
+        /**
+         * If the element is 100px offset from the top and 100 offset from the bottom
+         * set the class active to the section element, otherwise remove it.
+         */
+        if (top < 100 && top >= -Math.abs(offset - 100)) {
+            section.classList.add('your-active-class');
+        } else {
+            section.classList.remove('your-active-class');
+        }
+    }
+}
 
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToAnchor(id) {
+    document.querySelector(`#${id}`).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
 
 /**
  * End Main Functions
@@ -61,7 +79,12 @@ buildNavbar();
 */
 
 // Build menu 
-
-// Scroll to section on link click
+buildNavbar();
 
 // Set sections as active
+window.addEventListener('scroll', setActiveSectionOnScroll);
+
+// Remove event listeners
+window.onbeforeunload = () => {
+    window.removeEventListener('scroll', setActiveSectionOnScroll);
+};
